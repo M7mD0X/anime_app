c5ca2a7770f72241bc4f0477bbb4ebfec26378b0
Muhammed <85457690+M7mD0X@users.noreply.github.com>
2026-02-19 02:16:52 +0300

    update lib/screens/home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List animeList = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAnime();
+  }
+
+  Future<void> fetchAnime() async {
+    const url = 'https://graphql.anilist.co';
+    const query = '''
+    {
+      Page(perPage: 20) {
+        media(type: ANIME, sort: TRENDING_DESC) {
+          id
+          title { romaji arabic }
+          coverImage { large }
+          episodes
+          averageScore
+        }
+      }
+    }
+    ''';
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'query': query}),
+    );
+
+    final data = jsonDecode(response.body);
+    setState(() {
+      animeList = data['data']['Page']['media'];
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1A1A1A),
+        title: Text(
+          'أنمي سلاير',
+          style: TextStyle(
+            color: Color(0xFFE53935),
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator(color: Color(0xFFE53935)))
+          : GridView.builder(
+              padding: EdgeInsets.all(10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.65,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: animeList.length,
+              itemBuilder: (context, index) {
+                final anime = animeList[index];
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFF1A1A1A),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(10)),
+                        child: Image.network(
+                          anime['coverImage']['large'],
+                          height: 180,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                          anime['title']['arabic'] ?? anime['title']['romaji'],
+                          style: TextStyle(color: Colors.white, fontSize: 13),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: Text(
+                          '⭐ ${anime['averageScore'] ?? 'N/A'}',
+                          style: TextStyle(
+                              color: Color(0xFFE53935), fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
