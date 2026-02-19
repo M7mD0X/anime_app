10fc6de90d6873298ae78f581cf5a7a636cd9031
Muhammed <85457690+M7mD0X@users.noreply.github.com>
2026-02-19 13:01:17 +0300

    update lib/screens/home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List animeList = [];
   bool isLoading = true;
+  String errorMsg = '';
 
   @override
   void initState() {
@@ -21,40 +22,44 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<void> fetchAnime() async {
     try {
-      const url = 'https://graphql.anilist.co';
-      const query = '''
-      {
-        Page(perPage: 20) {
-          media(type: ANIME, sort: TRENDING_DESC) {
-            id
-            title { romaji arabic }
-            coverImage { large }
-            episodes
-            averageScore
-          }
-        }
-      }
-      ''';
-
       final response = await http.post(
-        Uri.parse(url),
+        Uri.parse('https://graphql.anilist.co'),
         headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'query': query}),
-      ).timeout(Duration(seconds: 10));
+        body: jsonEncode({
+          'query': '''
+          {
+            Page(perPage: 20) {
+              media(type: ANIME, sort: TRENDING_DESC) {
+                id
+                title { romaji arabic }
+                coverImage { large }
+                averageScore
+              }
+            }
+          }
+          '''
+        }),
+      );
 
-      final data = jsonDecode(response.body);
-      setState(() {
-        animeList = data['data']['Page']['media'];
-        isLoading = false;
-      });
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        setState(() {
+          animeList = data['data']['Page']['media'];
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          errorMsg = 'خطأ: ${response.statusCode}';
+          isLoading = false;
+        });
+      }
     } catch (e) {
       setState(() {
+        errorMsg = 'تعذر الاتصال: $e';
         isLoading = false;
       });
-      print('Error: $e');
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -62,68 +67,100 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: Color(0xFF1A1A1A),
         title: Text(
-          'Anime Os',
+          'أنمي سلاير',
           style: TextStyle(
             color: Color(0xFFE53935),
             fontWeight: FontWeight.bold,
             fontSize: 22,
           ),
         ),
-        centerTitle: false,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh, color: Colors.white),
+            onPressed: () {
+              setState(() { isLoading = true; errorMsg = ''; });
+              fetchAnime();
+            },
+          )
+        ],
       ),
       body: isLoading
           ? Center(child: CircularProgressIndicator(color: Color(0xFFE53935)))
-          : GridView.builder(
-              padding: EdgeInsets.all(10),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.65,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: animeList.length,
-              itemBuilder: (context, index) {
-                final anime = animeList[index];
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFF1A1A1A),
-                  ),
+          : errorMsg.isNotEmpty
+              ? Center(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(10)),
-                        child: Image.network(
-                          anime['coverImage']['large'],
-                          height: 180,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Text(
-                          anime['title']['arabic'] ?? anime['title']['romaji'],
-                          style: TextStyle(color: Colors.white, fontSize: 13),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(
-                          '⭐ ${anime['averageScore'] ?? 'N/A'}',
-                          style: TextStyle(
-                              color: Color(0xFFE53935), fontSize: 12),
-                        ),
-                      ),
+                      Icon(Icons.error, color: Colors.red, size: 60),
+                      SizedBox(height: 10),
+                      Text(errorMsg, style: TextStyle(color: Colors.white)),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() { isLoading = true; errorMsg = ''; });
+                          fetchAnime();
+                        },
+                        child: Text('إعادة المحاولة'),
+                      )
                     ],
                   ),
-                );
-              },
-            ),
+                )
+              : GridView.builder(
+                  padding: EdgeInsets.all(10),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.65,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: animeList.length,
+                  itemBuilder: (context, index) {
+                    final anime = animeList[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF1A1A1A),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(10)),
+                            child: Image.network(
+                              anime['coverImage']['large'],
+                              height: 180,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                              errorBuilder: (c, e, s) => Container(
+                                height: 180,
+                                color: Colors.grey[900],
+                                child: Icon(Icons.broken_image, color: Colors.grey),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Text(
+                              anime['title']['arabic'] ?? anime['title']['romaji'],
+                              style: TextStyle(color: Colors.white, fontSize: 13),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8),
+                            child: Text(
+                              '⭐ ${anime['averageScore'] ?? 'N/A'}',
+                              style: TextStyle(color: Color(0xFFE53935), fontSize: 12),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
     );
   }
 }
